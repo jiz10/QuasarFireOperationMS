5e56b3288282257f3fea82ad07c99f59337959b1
jizambrana <jose.zambrana@globallogic.com>
2021-04-28 17:01:30 -0300

    Adding spring structure

---
+++ src/main/java/com/QuasarFireOperationMS/QuasarFireOperationMsApplication.java
@@ -0,0 +1,13 @@
+package com.QuasarFireOperationMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuasarFireOperationMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuasarFireOperationMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/QuasarFireOperationMS/QuasarFireOperationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuasarFireOperationMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuasarFireOperationMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
