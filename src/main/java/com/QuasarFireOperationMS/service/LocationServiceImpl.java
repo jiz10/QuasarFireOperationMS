97ead99bf4effe2297e0572a1719f156dbb6268d
jizambrana <jose.zambrana@globallogic.com>
2021-04-30 22:02:54 -0300

    Adding getLocation method

--- src/main/java/com/QuasarFireOperationMS/service/LocationServiceImpl.java
+++ src/main/java/com/QuasarFireOperationMS/service/LocationServiceImpl.java
@@ -1,10 +1,17 @@
 package com.QuasarFireOperationMS.service;
 
+import com.QuasarFireOperationMS.util.LocationCalculator;
 import com.QuasarFireOperationMS.web.model.LocationInfoDto;
 import com.QuasarFireOperationMS.web.model.PositionDto;
+import com.QuasarFireOperationMS.web.model.SatelliteDto;
 import com.QuasarFireOperationMS.web.model.SatellitesDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author jiz10
@@ -14,10 +21,38 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class LocationServiceImpl implements LocationService {
 
+    @Autowired
+    LocationCalculator locationCalculator;
+
+    @Value("${satellites.names}")
+    private String[] satellitesNames;
+
     @Override
     public LocationInfoDto calculateLocationFromSatellitesGroup(SatellitesDto satellitesDto) {
 
-        return LocationInfoDto.builder().message("Mensaje de prueba").position(PositionDto.builder().x(100.0).y(100.0).build()).build();
+
+
+        double distanceOne = 0;
+        double distanceTwo = 0;
+        double distanceThree = 0;
+
+        List<String> satellitesNamesList = Arrays.asList(satellitesNames);
+        for (SatelliteDto sat : satellitesDto.getSatellites()) {
+            System.out.println(sat.getName());
+            if (sat.getName().equalsIgnoreCase(satellitesNamesList.get(0))) {
+                distanceOne = sat.getDistance();
+                System.out.println(distanceOne);
+            } else if (sat.getName().equalsIgnoreCase(satellitesNamesList.get(1))) {
+                distanceTwo = sat.getDistance();
+                System.out.println(distanceTwo);
+            } else if (sat.getName().equalsIgnoreCase(satellitesNamesList.get(2))) {
+                distanceThree = sat.getDistance();
+                System.out.println(distanceThree);
+            }
+        }
+        double[] distances = new double[]{distanceOne, distanceTwo, distanceThree};
+        PositionDto positionDto = locationCalculator.getLocation(distances);
+        return LocationInfoDto.builder().message("Mensaje de prueba").position(positionDto).build();
 
     }
 }
---
+++ src/main/java/com/QuasarFireOperationMS/util/LocationCalculator.java
@@ -0,0 +1,61 @@
+package com.QuasarFireOperationMS.util;
+
+import com.QuasarFireOperationMS.web.model.PositionDto;
+import com.lemmingapex.trilateration.NonLinearLeastSquaresSolver;
+import com.lemmingapex.trilateration.TrilaterationFunction;
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresOptimizer;
+import org.apache.commons.math3.fitting.leastsquares.LevenbergMarquardtOptimizer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author jiz10
+ * 30/4/21
+ */
+@Service
+public class LocationCalculator {
+
+    @Value("#{new Double('${satellites.one.x}')}")
+    private Double satOneX;
+
+    @Value("#{new Double('${satellites.one.y}')}")
+    private Double satOneY;
+
+    @Value("#{new Double('${satellites.two.x}')}")
+    private Double satTwoX;
+
+    @Value("#{new Double('${satellites.two.y}')}")
+    private Double satTwoY;
+
+    @Value("#{new Double('${satellites.three.x}')}")
+    private Double satThreeX;
+
+    @Value("#{new Double('${satellites.three.y}')}")
+    private Double satThreeY;
+
+    public PositionDto getLocation(double[] distances) {
+        //double[][] positions2 = new double[][]{{37.418436, -121.963477}, {37.417243, -121.961889}, {37.418692, -121.960194}};
+        //double[] distances2 = new double[]{0.265710701754, 0.234592423446, 0.0548954278262};
+
+        double[][] positions = new double[][]{{satOneX, satOneY}, {satTwoX, satTwoY}, {satThreeX, satThreeY}};
+        //double[][] positions = new double[][]{{37.418436, -121.963477}, {37.417243, -121.961889}, {37.418692, -121.960194}};
+        //double[] distances = new double[]{0.265710701754, 0.234592423446, 0.0548954278262};
+
+        NonLinearLeastSquaresSolver solver = new NonLinearLeastSquaresSolver(new TrilaterationFunction(positions, distances), new LevenbergMarquardtOptimizer());
+        LeastSquaresOptimizer.Optimum optimum = solver.solve();
+
+        //the answer
+        double[] centroid = optimum.getPoint().toArray();
+        System.out.println(centroid[0]);
+        System.out.println(centroid[1]);
+
+        return PositionDto.builder().x(centroid[0]).y(centroid[1]).build();
+    }
+    /*
+    {
+    "position": {
+        "x": -58.315252587138595,
+        "y": -69.55141837312165
+    },
+    */
+}
